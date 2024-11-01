2c7c4a6433b6776816073e3537b937a42a9c6e37
CoffeeStraw <clarantonio98@gmail.com>
2024-11-01 21:49:51 +0100

    Add prettier and formatted all the files (yarn format), add some documentation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import { scanUrlsStore, loadScansData } from '$lib/stores/scans';
-    import BrainScans from '$lib/components/BrainScans.svelte';
+	import { onMount } from 'svelte';
+	import { scanUrlsStore, loadScansData } from '$lib/stores/scans';
+	import BrainScans from '$lib/components/BrainScans.svelte';
 
-    onMount(() => {
-        loadScansData();
-    });
+	onMount(() => {
+		loadScansData();
+	});
 </script>
 
+<svelte:head>
+	<title>VoxLogica UI</title>
+	<meta name="description" content="View and analyze brain scan images" />
+</svelte:head>
+
 <div class="container mx-auto p-4">
-    {#if $scanUrlsStore}
-        <BrainScans scanData={$scanUrlsStore} />
-    {:else}
-        <div class="flex justify-center items-center h-32">
-            <p class="text-lg">Loading scans...</p>
-        </div>
-    {/if}
+	{#if $scanUrlsStore}
+		<BrainScans scanData={$scanUrlsStore} />
+	{:else}
+		<div class="flex justify-center items-center h-32">
+			<p class="text-lg">Loading scans...</p>
+		</div>
+	{/if}
 </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,17 @@
-import { join } from 'path'
-import type { Config } from 'tailwindcss'
-import { skeleton } from '@skeletonlabs/tw-plugin'
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
+import { skeleton } from '@skeletonlabs/tw-plugin';
 
+/**
+ * Tailwind configuration with Skeleton UI integration
+ * Supports dark mode and custom theme presets (skeleton and hamlindigo)
+ */
 export default {
 	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}'),
+	],
 	theme: {
 		extend: {},
 	},
@@ -19,7 +26,7 @@ export default {
 					{
 						name: 'hamlindigo',
 						enhancements: true,
-					}
+					},
 				],
 			},
 		}),
