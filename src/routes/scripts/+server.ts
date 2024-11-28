cc2b71ef01fd99980a3b2ba5c45b585f187a9e6a
CoffeeStraw <clarantonio98@gmail.com>
2024-11-28 21:46:18 +0100

    Update script fetching with correct typing for script

--- src/routes/scripts/+server.ts
+++ src/routes/scripts/+server.ts
@@ -1,14 +1,13 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import fs from 'fs/promises';
-import path from 'path';
 import { SCRIPTS_PATH } from '../config';
-import type { Script } from '$lib/models/types';
+import type { PresetScript } from '$lib/models/types';
 
 export const GET: RequestHandler = async () => {
 	try {
 		const files = await fs.readdir(SCRIPTS_PATH);
-		const scripts: Script[] = files
+		const scripts: PresetScript[] = files
 			.filter((file) => file.endsWith('.imgql'))
 			.map((file) => ({
 				id: file,
