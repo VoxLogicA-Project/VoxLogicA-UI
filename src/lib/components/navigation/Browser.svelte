5e43e9b7f64c5466484eb62359320bad70c0b86f
CoffeeStraw <clarantonio98@gmail.com>
2024-12-10 19:36:52 +0100

    Enhance case selection display for better visibility

--- src/lib/components/navigation/Browser.svelte
+++ src/lib/components/navigation/Browser.svelte
@@ -95,8 +95,11 @@
 		<div class="flex items-center justify-between gap-2">
 			<span class="text-xl font-bold tracking-wide text-primary-900-50-token truncate">Browser</span
 			>
-			<div class="text-sm text-surface-600-300-token whitespace-nowrap">
-				{caseViewModel.selectedCases.length}/{caseViewModel.MAX_SELECTED_CASES} cases selected
+			<div class="flex-none whitespace-nowrap text-sm">
+				<span class="font-bold text-primary-500">{caseViewModel.selectedCases.length}</span>
+				<span class="text-surface-600-300-token"
+					>/ {caseViewModel.MAX_SELECTED_CASES} cases selected</span
+				>
 			</div>
 		</div>
 	</header>
